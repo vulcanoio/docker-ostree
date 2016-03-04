80499c1f5bffe7f635a419451c7deca691933e8e
Petros Angelatos <petrosagg@gmail.com>
2016-03-03 22:21:09 -0800

    remove temporary container after each command ends
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- commit.sh
+++ commit.sh
@@ -8,6 +8,6 @@ container=$(docker create $image /bin/false)
 
 cmd="LC_ALL=C.UTF-8 ostree -v commit -v --tar-autocreate-parents --tree=tar=<(cat) --subject $image --branch $branch"
 
-docker export $container | docker run -i -v $repo:/ostree/repo resin/ostree bash -c "$cmd"
+docker export $container | docker run --rm -i -v $repo:/ostree/repo resin/ostree bash -c "$cmd"
 
 docker rm $container
--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ repo=$(readlink -f $1)
 
 cmd="ostree admin init-fs /"
 
-docker run -v $repo:/ostree/repo resin/ostree $cmd
+docker run --rm -v $repo:/ostree/repo resin/ostree $cmd
